d941ac56f1122f529f1075a9dcbdba1c0438d07b
cfizette <cfizett1@binghamton.edu>
2018-12-02 18:29:13 -0500

    Finished notebook utils and added feature set performance anlaysis

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,9 @@
 import itertools
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, log_loss
+import numpy as np
+import pandas as pd
+
 '''
 Collection of functions for metric reporting.
 '''
@@ -51,3 +54,29 @@ def plot_confusion_matrix(cm, classes,
 def calc_performance(y_true, y_pred, y_pred_proba):
     cross_ent = log_loss(y_true, y_pred_proba)
     accuracy = accuracy_score(y_true, y_pred)
+    result_list = [cross_ent, accuracy]
+    indices = ['cross_entropy', 'accuracy']
+    results = pd.DataFrame(result_list, index=indices, columns=['Score'])
+    return results
+
+
+def calc_test_train_performance(estimator, x_train, x_test, y_train, y_test, binary=False):
+    y_pred_train = estimator.predict(x_train)
+    y_pred_test = estimator.predict(x_test)
+    y_proba_train = estimator.predict_proba(x_train)
+    y_proba_test = estimator.predict_proba(x_test)
+    if binary:
+        y_proba_train = y_proba_train[:,1]
+        y_proba_test = y_proba_test[:,1]
+        
+    train_perf = calc_performance(y_train, y_pred_train, y_proba_train)
+    test_perf = calc_performance(y_test, y_pred_test, y_proba_test)
+    
+    # Should rename this to all_perf or something for clarity.... oh well
+    train_perf.columns = ['Train_Score']
+    train_perf['Test_Score'] = test_perf['Score']
+    
+    # Train perf now contains both train and test performance
+    return train_perf
+
+
